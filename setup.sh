78e76a3c79aee7b809df1ba0ffce68d5b3782962
Jie Chen <jiechenmc@gmail.com>
2024-05-10 14:47:37 -0400

    init

--- setup.sh
+++
@@ -1,8 +0,0 @@
-# Installing build tools
-sudo apt-get install libssl-dev libuv1-dev cmake make
-
-# Installing Ansible
-sudo apt update
-sudo apt install software-properties-common
-sudo add-apt-repository --yes --update ppa:ansible/ansible
-sudo apt install ansible
